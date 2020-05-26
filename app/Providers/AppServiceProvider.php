9d671c8758c7d58226cb7c77b3163e4608105a72
Fernando Binasco <fernando.binasco@gmail.com>
2020-05-26 15:49:51 -0300

    adicionado facades helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(\Faker\Generator::class, function(){
             return \Faker\Factory::create('pt_BR');
         });
+
+        // facades helper
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
     }
 
     /**
